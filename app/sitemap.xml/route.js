1d3938d4f1390f70d6d908d72d2a48e35f715b9b
prachi-max <prachi.nawale09@gmail.com>
2025-07-01 23:06:26 +0530

    Fix sitemap model import

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,9 +1,31 @@
+// app/sitemap.xml/route.js
+
+import { NextResponse } from 'next/server';
+import Article from '@/models/Article';
+import connectDB from '@/lib/mongodb';
+
 export async function GET() {
-  const articles = await Article.find(); // or fetch from API
-  const urls = articles.map(a => `<url><loc>https://yourdomain.com/article/${a.slug}</loc></url>`).join('');
+  await connectDB();
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`;
+  const articles = await Article.find();
 
-  return new Response(xml, { headers: { 'Content-Type': 'application/xml' } });
+  const urls = articles.map((article) => {
+    return `
+      <url>
+        <loc>https://your-domain.com/article/${article.slug}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+      </url>`;
+  });
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls.join('')}
+    </urlset>`;
+
+  return new NextResponse(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
 }
